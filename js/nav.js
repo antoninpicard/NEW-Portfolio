82efb1d6e63d7a86085b0ba55518fdbbb3fcd054
Antonin <antoninpicard0@gmail.com>
2022-11-22 15:07:50 +0100

    responsive loading

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,12 @@
 var header = document.getElementsByTagName('header')[0];
         header.style.backgroundColor = 'transparent';
 
-        // trigger this function every time the user scrolls
         window.onscroll = function (event) {
             var scroll = window.pageYOffset;
             if (scroll < 150) {
-                // green
+
                 header.style.backgroundColor = 'transparent';
             } else {
-                // purple
                 header.style.backgroundColor = 'black';
             }
         }
