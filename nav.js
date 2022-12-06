b7f85f85ab736ac85418bd6d0688c93c184a80ad
Antonin <antoninpicard0@gmail.com>
2022-12-06 17:01:56 +0100

    update

---
+++ nav.js
@@ -0,0 +1,12 @@
+var header = document.getElementsByTagName('header')[0];
+        header.style.backgroundColor = 'transparent';
+
+        window.onscroll = function (event) {
+            var scroll = window.pageYOffset;
+            if (scroll < 150) {
+
+                header.style.backgroundColor = 'transparent';
+            } else {
+                header.style.backgroundColor = 'black';
+            }
+        }
